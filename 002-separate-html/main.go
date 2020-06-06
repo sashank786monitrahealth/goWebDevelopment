5d3d8aac9f833411e7f065763f0d9217c380f759
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-06 20:00:21 +0530

    separated template file

---
+++ 002-separate-html/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("tpl.gohtml")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
