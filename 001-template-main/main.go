43c29dd3e7a4b76656a9abb5f3d351dee96988f9
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-06 19:52:24 +0530

    simple html template in go

---
+++ 001-template-main/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string = "Hey There! It is me."
+
+	var tpl string = `
+	<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Demo-Template</title>
+</head>
+<body>
+    <h1>` + name + `</h1>
+</body>
+</html>
+	`
+
+	fmt.Printf(" %v\n", tpl)
+}
