b4c0fd947cd00e553131132b101393ca471070e5
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-27 12:37:22 +0530

    evaluate scores

---
+++ 023-evaluate-scores/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+
+	g1 := struct {
+		Score1 int
+		Score2 int
+	}{
+		7,
+		9,
+	}
+
+	var err error = tpl.Execute(os.Stdout, g1)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+}
