d314ffcacb74c6c0d5ed350351dddf334cb25e41
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-06-30 07:34:16 +0530

    tcp server

---
+++ 029-tcp-server-go/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", ":8080")
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	defer li.Close()
+
+	for {
+		conn, err := li.Accept()
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		io.WriteString(conn, "\nHello from TCP server\n")
+		fmt.Fprintln(conn, "How is your day?")
+		fmt.Fprintf(conn, "%v", "Well, I hope!")
+
+		conn.Close()
+	}
+}
+
+/*
+Run server:
+go run main.go
+*/
+
+/*
+Check output with CURL
+=============
+curl localhost:8080
+
+*/
