bedcd54fde58218f682ffb5ddddc899fa41d048d
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-04 18:40:04 +0530

    in memory db simulation

--- 040-in-memory-database/main.go
+++ 040-in-memory-database/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -42,5 +45,41 @@ func handle(conn net.Conn) {
 		"EXAMPLE:\r\n" +
 		"\tSET fav chocolate \r\n" +
 		"\t GET fav \r\n\r\n\r\n"
+
 	io.WriteString(conn, vWriteStr)
+
+	// read & write
+	var data map[string]string = make(map[string]string)
+	var scanner *bufio.Scanner = bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		var ln string = scanner.Text()
+		var fs []string = strings.Fields(ln)
+
+		//logic
+		if len(fs) < 1 {
+			continue
+		}
+
+		switch fs[0] {
+		case "GET":
+			var k string = fs[1]
+			var v string = data[k]
+			fmt.Fprintf(conn, "%s\r\n", v)
+		case "SET":
+			if len(fs) != 3 {
+				fmt.Fprintln(conn, "EXPECTED VALUE\r\n")
+				continue
+			}
+			var k string = fs[1]
+			var v string = fs[2]
+			data[k] = v
+		case "DEL":
+			var k string = fs[1]
+			delete(data, k)
+		default:
+			fmt.Fprintln(conn, "INVALID COMMAND "+fs[0]+"\r\n")
+			continue
+		}
+	}
 }
