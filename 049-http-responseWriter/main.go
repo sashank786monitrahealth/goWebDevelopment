7c8d71d06882abf6d388650c6bc3cc2292097e08
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-08 17:59:17 +0530

    text/plain

--- 049-http-responseWriter/main.go
+++ 049-http-responseWriter/main.go
@@ -9,7 +9,7 @@ type hotdog int
 
 func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Mcleod-Key", "this is from mcleod")
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	fmt.Fprintln(w, "<h1>Any code you want in this func</h1>")
 }
 
