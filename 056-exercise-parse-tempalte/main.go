e281847ca6d238e4e5952a2107f0b0e94318d425
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-13 01:24:25 +0530

    parse template

---
+++ 056-exercise-parse-tempalte/main.go
@@ -0,0 +1,46 @@
+/*
+1. Take the previous program in the previous folder and change it so that:
+   a) a template is parsed and served
+   b) you pass data into the template
+*/
+
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", foo)
+	http.HandleFunc("/dog/", bar)
+	http.HandleFunc("/me/", bhogu)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+func foo(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "foo ran")
+}
+
+func bar(res http.ResponseWriter, req *http.Request) {
+	io.WriteString(res, "bar ran")
+}
+
+func bhogu(res http.ResponseWriter, req *http.Request) {
+	var tpl *template.Template
+	var err error
+	tpl, err = template.ParseFiles("something.gohtml")
+
+	if err != nil {
+		log.Fatalln("error parsing template", err)
+	}
+
+	err = tpl.ExecuteTemplate(res, "something.gohtml", "Bhogu")
+
+	if err != nil {
+		log.Fatalln("error executing template", err)
+	}
+}
