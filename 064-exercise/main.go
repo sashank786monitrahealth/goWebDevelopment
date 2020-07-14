e77617e02b4bdb2d9df124453b5c67bfe0f9842c
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-15 03:25:07 +0530

    read method and uri

--- 064-exercise/main.go
+++ 064-exercise/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -36,25 +37,40 @@ func main() {
 func serve(conn net.Conn) {
 
 	defer conn.Close()
-
+	var i int64
+	var rMethod, rURI string
 	var scanner *bufio.Scanner = bufio.NewScanner(conn)
 	for scanner.Scan() {
 		var ln string = scanner.Text()
 		fmt.Println(ln)
+
+		if i == 0 {
+			// we are in REQUEST LINE
+			var xs []string = strings.Fields(ln)
+			rMethod = xs[0]
+			rURI = xs[1]
+			fmt.Println("METHOD:", rMethod)
+			fmt.Println("URI:", rURI)
+		}
 
 		if ln == "" {
 			// when ln is empty, header is done
 			fmt.Println("This is the end of the http request headers.")
 			break
 		}
+		i++
 	}
 
 	var body string = "CHECK OUT THE RESPONSE BODY PAYLOAD"
+	body += "\n"
+	body += rMethod
+	body += "\n"
+	body += rURI
 
 	// we never get here
 	// we have an open stream connection
 	// how does the above reader know when it is done?
-	fmt.Println("Code got here")
+	//fmt.Println("Code got here")
 	io.WriteString(conn, "HTTP/1.1 200 OK\r\n")
 	fmt.Fprintf(conn, "Content-Length: %d\r\n", len(body))
 	fmt.Fprintf(conn, "Content-Type: text/plain\r\n")
