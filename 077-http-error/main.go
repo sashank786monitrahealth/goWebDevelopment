5590e29b281ee5cfb45bac4eeb41fdca5a96deac
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-20 15:26:23 +0530

    404 - file not fond

--- 077-http-error/main.go
+++ 077-http-error/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", dog)
@@ -9,9 +13,29 @@ func main() {
 }
 
 func dog(w http.ResponseWriter, req *http.Request) {
-
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `<img src="/toby.jpg">`)
 }
 
 func dogPic(w http.ResponseWriter, req *http.Request) {
+	var f *os.File
+	var err error
 
+	f, err = os.Open("toby.jpg")
+
+	if err != nil {
+		http.Error(w, "file not found1.", http.StatusNotFound)
+		return
+	}
+
+	defer f.Close()
+
+	var fi os.FileInfo
+	fi, err = f.Stat()
+
+	if err != nil {
+		http.Error(w, "file not found1", 404)
+	}
+
+	http.ServeContent(w, req, f.Name(), fi.ModTime(), f)
 }
