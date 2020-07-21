66dfc960e0a19c2200f29479d70596ee5dffd5bc
Sashank Bhogu <sashank.bhogu@monitrahealth.com>
2020-07-21 18:39:45 +0530

    handle not found favicon

---
+++ 083-not-found-favicon/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", dog)
+	//http.Handle("favicon.ico", http.NotFoundHandler())
+	http.ListenAndServe(":8080", nil)
+}
+
+func dog(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.URL)
+	fmt.Fprintln(w, "go look at your terminal")
+}
